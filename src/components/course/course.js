ec00af2abee452d12127b9e3e481b843823d8357
zenon191 <ngocnghia19011999@gmail.com>
2019-12-12 22:40:46 +0700

    test courses

--- src/components/course/course.js
+++ src/components/course/course.js
@@ -1,28 +1,77 @@
-import React from 'react';
-import Select from 'react-select';
-import './course.css'
+import React from "react";
+import Select from "react-select";
+import "./course.css";
 
-export default class Course extends React.Component{
-    render() {
-        return (
-            <div className="course">
-                <div className="course-header">
-                    <div className="header-items"><input className="form-control" placeholder="Mã môn học hoặc tên môn học." type="text"/></div>
-                    <div className="header-items"><Select/></div>
-                    <div className="header-items"><input className="btn btn-primary" type="button" value={"Create new"}/></div>
-                </div>
-                <div className="course-content">
-                    <table className="table">
-                        <thead>
-                        <tr>
-                            <th>STT</th>
-                            <th>Course number</th>
-                            <th>Course name</th>
-                        </tr>
-                        </thead>
-                    </table>
-                </div>
-            </div>
-        )
-    }
+const options = [
+  { value: "Hoc ky 1 - NH:2019-2020", label: "Hoc ky 1 - NH:2019-2020" },
+  { value: "Hoc ky 2 - NH:2019-2020", label: "Hoc ky 2 - NH:2019-2020" }
+];
+const courses = [
+  { courses_num: "INT3302", name: "Phat trien ung dung web" },
+  { courses_num: "INT3304", name: "Lap trinh nang cao" }
+];
+export default class Course extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedOption: null,
+      courses:[]
+    };
+  }
+  handleChange = (selectedOption) => {
+      this.setState({selectedOption});
+  };
+  render() {
+      const {selectedOption} = this.state;
+    return (
+      <div className="course">
+        <div className="course-header">
+          <div className="header-items">
+            <input
+              className="form-control"
+              placeholder="Mã môn học hoặc tên môn học."
+              type="text"
+            />
+          </div>
+          <div className="header-items">
+            <Select 
+                value={selectedOption}
+                onChange={this.handleChange}
+                options={options}
+            />
+          </div>
+          <div className="header-items">
+            <input
+              className="btn btn-primary"
+              type="button"
+              value={"Create new"}
+            />
+          </div>
+        </div>
+        <div className="course-content">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>STT</th>
+                <th>Course number</th>
+                <th>Course name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {courses.map((e, index) => {
+                console.log(e.name);
+                return (
+                  <tr key={index}>
+                    <td>{++index}</td>
+                    <td>{e.courses_num}</td>
+                    <td>{e.name}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
